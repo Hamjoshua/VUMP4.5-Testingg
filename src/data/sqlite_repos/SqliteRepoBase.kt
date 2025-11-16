c9e029a0ee08907d2b803c0aa87469ead4e822f9
Biryukov Mark <72654119+Hamjoshua@users.noreply.github.com>
2025-11-16 19:30:22 +0700

    Фикс валидатора на реальную проверку подключения к БД

--- src/data/sqlite_repos/SqliteRepoBase.kt
+++ src/data/sqlite_repos/SqliteRepoBase.kt
@@ -1,11 +1,20 @@
 package org.example.data.sqlite_repos
 
-import java.sql.*;
+import java.sql.*
+import kotlin.io.path.exists
+import kotlin.io.path.Path
+
 
 open class SqliteRepoBase(dbFilePath: String) {
-    private lateinit var _connection : Connection
+    protected lateinit var _connection: Connection
 
     init {
+        // проверка на нормальное подключение к БД
+        val dbPath = Path(dbFilePath)
+        if (!dbPath.exists()){
+            throw SQLException("Not connected to db at address ${dbFilePath}")
+        }
+
         val fullPath = "jdbc:sqlite:${dbFilePath}"
         _connection = DriverManager.getConnection(fullPath)
     }
