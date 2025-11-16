f49e51c6e835a76ccd49fd07c59a8c825ab6c03a
Biryukov Mark <72654119+Hamjoshua@users.noreply.github.com>
2025-11-16 16:54:21 +0700

    Добавление интерфейсов для репозитория

---
+++ src/data/interfaces/IUsersRepo.kt
@@ -0,0 +1,10 @@
+package org.example.data.interfaces
+import org.example.entities.User
+
+interface IUsersRepo {
+    fun getById(id: Int): User?
+    fun getAll(): List<User>
+    fun add(user: User): Boolean
+    fun update(user: User): Boolean
+    fun delete(id: Int): Boolean
+}
