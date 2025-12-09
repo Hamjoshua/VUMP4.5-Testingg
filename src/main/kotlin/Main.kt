7b855a5565bb50e46cff9d8f1dc5b451161f6a68
Biryukov Mark <72654119+Hamjoshua@users.noreply.github.com>
2025-12-09 08:34:29 +0700

    Точка входа SpringBoot

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,30 +3,41 @@ import kotlinx.cli.*
 import org.example.entities.StatusCode
 import org.example.services.AccessControlService
 import org.example.validators.ValidationContext
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
 import kotlin.system.exitProcess
 
-fun main(args: Array<String>) {
-    val parser = ArgParser("example")
-    val login by parser.option(ArgType.String, shortName = "l", description = "User login").required()
-    val password by parser.option(ArgType.String, shortName = "p", description = "User password").required()
-    val resource by parser.option(ArgType.String, shortName = "r", description = "Path to resource").required()
-    val action by parser.option(ArgType.String, shortName = "a", description = "Requested action").required()
-    val volume by parser.option(ArgType.Int, shortName = "v", description = "Volume of used resource").required()
-    try {
-        parser.parse(args)
-    } catch (e: Exception) {
-        val helpArg = args + "-h"
-        parser.parse(helpArg) // я буквально не придумал ничего лучше. Господи прости за это... да нормуль!!
-        exitProcess(StatusCode.HELP_REQUESTED.code)
+@SpringBootApplication
+class SpringBootConsoleApplication: CommandLineRunner {
+    override fun run(vararg args: String) {
+        val parser = ArgParser("example")
+        val login by parser.option(ArgType.String, shortName = "l", description = "User login").required()
+        val password by parser.option(ArgType.String, shortName = "p", description = "User password").required()
+        val resource by parser.option(ArgType.String, shortName = "r", description = "Path to resource").required()
+        val action by parser.option(ArgType.String, shortName = "a", description = "Requested action").required()
+        val volume by parser.option(ArgType.Int, shortName = "v", description = "Volume of used resource").required()
+        try {
+            parser.parse(args)
+        } catch (e: Exception) {
+            // val helpArg = args + "-h"
+            // parser.parse(helpArg) // я буквально не придумал ничего лучше. Господи прости за это... да нормуль!!
+            // exitProcess(StatusCode.HELP_REQUESTED.code)
+        }
+
+        val context = ValidationContext(
+            login = login,
+            password = password,
+            resourcePath = resource,
+            action = action,
+            volume = volume
+        )
+
+        exitProcess(AccessControlService().checkAccess(context).code)
     }
+}
 
-    val context = ValidationContext(
-        login = login,
-        password = password,
-        resourcePath = resource,
-        action = action,
-        volume = volume
-    )
 
-    exitProcess(AccessControlService().checkAccess(context).code)
+fun main(args: Array<String>) {
+    runApplication<AccessControlService>(*args)
 }
